4bd978d9d0e465da5e523e633e5852a55969c916
Ben Dunn <benthedunn@gmail.com>
2020-12-07 15:08:06 -0700

    random potions

--- src/components/shelves.tsx
+++ src/components/shelves.tsx
@@ -3,19 +3,20 @@ import '../App.css';
 
 const Shelves = ({potions}) => {
   
-  let randomPotion = potions[Math.floor(Math.random() * potions.length)];
-  console.log(randomPotion)
+  let randomPotion1 = potions[Math.floor(Math.random() * potions.length)];
+  let randomPotion2 = potions[Math.floor(Math.random() * potions.length)];
+  let randomPotion3 = potions[Math.floor(Math.random() * potions.length)];
 
   return (
     <div className="shelves">
       <div className="spot1">
-        <img className="potion" src={randomPotion.name} alt="potion"/>
+        <img className="potion" src={randomPotion1.name} alt="potion"/>
       </div>
       <div className="spot2">
-        <img className="potion" src={randomPotion.name} alt="potion"/>
+        <img className="potion" src={randomPotion2.name} alt="potion"/>
       </div>
       <div className="spot3">
-        <img className="potion" src={randomPotion.name} alt="potion"/>
+        <img className="potion" src={randomPotion3.name} alt="potion"/>
       </div>
       <div className="recolor"></div>
 
